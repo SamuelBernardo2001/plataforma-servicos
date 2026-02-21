a08d157d89e38cbb288082ee30afe8ff089f40e9
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-02-20 23:51:09 -0300

    iniciando Projeto

---
+++ src/main/java/com/plataforma/servicos/PlataformaServicosApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.servicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaServicosApplication.class, args);
+	}
+
+}
