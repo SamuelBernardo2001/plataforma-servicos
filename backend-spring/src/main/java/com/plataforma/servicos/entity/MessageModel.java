031a50027a7305e890f42c85c1859260df3cb3c0
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-20 01:14:53 -0300

    Entidades convertidas para JPA com sucesso.

--- backend-spring/src/main/java/com/plataforma/servicos/entity/MessageModel.java
+++ backend-spring/src/main/java/com/plataforma/servicos/entity/MessageModel.java
@@ -1,5 +1,6 @@
 package com.plataforma.servicos.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,18 +12,27 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "messages")
 public class MessageModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private UUID serviceOrderId;
 
+    @Column(nullable = false)
     private UUID remetenteId;
 
+    @Column(nullable = false)
     private UUID receptorId;
 
+    @Column(nullable = false, columnDefinition = "TEXT") // Permite mensagens longas
     private String conteudo;
 
+    @Column(nullable = false)
     private Boolean ler;
 
     private LocalDateTime enviadoEm;
