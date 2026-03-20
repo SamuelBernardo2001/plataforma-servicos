97031496c80d7d6bd5bc8010a1a8f92c5a197af2
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-20 01:24:42 -0300

    Entidades convertidas para JPA com sucesso.

--- backend-spring/src/main/java/com/plataforma/servicos/entity/ServiceOrderModel.java
+++ backend-spring/src/main/java/com/plataforma/servicos/entity/ServiceOrderModel.java
@@ -1,5 +1,6 @@
 package com.plataforma.servicos.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -12,19 +13,28 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "service_orders")
 public class ServiceOrderModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private UUID serviceId;
 
+    @Column(nullable = false)
     private UUID clientId;
 
+    @Column(nullable = false)
     private UUID provedorId;
 
+    @Column(nullable = false)
     private BigDecimal price;
 
     @Builder.Default
+    @Column(nullable = false)
     private OrderStatusEnum status = OrderStatusEnum.REQUESTED;
 
     private LocalDateTime criadoEm;
