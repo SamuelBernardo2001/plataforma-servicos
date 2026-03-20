5259aedfb251b54b32b315f426007af82cb98a0b
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-20 01:10:07 -0300

    Entidades convertidas para JPA com sucesso.

--- backend-spring/src/main/java/com/plataforma/servicos/entity/CategoryModel.java
+++ backend-spring/src/main/java/com/plataforma/servicos/entity/CategoryModel.java
@@ -1,5 +1,6 @@
 package com.plataforma.servicos.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,12 +12,18 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "categories")
 public class CategoryModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false, unique = true)
     private String nome; // (Ex: Elétrica, Encanamento)
 
+    @Column(length = 1000)
     private String descricao;
 
     @Builder.Default
