36fc38444ca3b8a2c5ae0a72e3a3c0697d5d5e3c
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-20 01:12:32 -0300

    Entidades convertidas para JPA com sucesso.

--- backend-spring/src/main/java/com/plataforma/servicos/entity/FavoriteModel.java
+++ backend-spring/src/main/java/com/plataforma/servicos/entity/FavoriteModel.java
@@ -1,5 +1,6 @@
 package com.plataforma.servicos.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,12 +12,18 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "favorites")
 public class FavoriteModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private UUID userId;
 
+    @Column(nullable = false)
     private UUID serviceId;
 
     private LocalDateTime criadoEm;
