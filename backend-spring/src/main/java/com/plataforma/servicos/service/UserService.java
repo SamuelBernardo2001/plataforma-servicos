52f285da730b5c50da516a52cbdb0e02ad47d881
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-30 01:26:34 -0300

    adicionadno a injeção do userRepository para acessar o banco e o mapper para  conversoes

--- backend-spring/src/main/java/com/plataforma/servicos/service/UserService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/UserService.java
@@ -1,9 +1,20 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.dto.UserDTOS.UserResponseDTO;
+import com.plataforma.servicos.entity.UserModel;
+import com.plataforma.servicos.mapper.UserMapper;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-}
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    }
+
