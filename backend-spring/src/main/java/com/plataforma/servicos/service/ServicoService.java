a5d4387330a558b6fc6017e031f7cd6dad793626
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-30 19:53:46 -0300

    fazendo a injeção de dependencia dos repositorios para o serviços, usuarios, categoria e para converter as entidades

--- backend-spring/src/main/java/com/plataforma/servicos/service/ServicoService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/ServicoService.java
@@ -1,9 +1,19 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.mapper.ServiceMapper;
+import com.plataforma.servicos.repository.CategoryRepository;
+import com.plataforma.servicos.repository.ServiceRepository;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ServicoService {
+
+    private final ServiceRepository serviceRepository;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final ServiceMapper serviceMapper;
+
 }
