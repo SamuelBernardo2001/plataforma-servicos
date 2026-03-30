c5f70e130fe1a478796b2a9adb3db64beef4dffc
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-30 19:56:32 -0300

    criando o metodo para busca do serviço por ID, verificando se ele existe e se está ativo. Se o serviço não existir ou estiver desativado, uma exceção é lançada. Caso contrário, o serviço é convertido para um DTO de resposta e retornado.

--- backend-spring/src/main/java/com/plataforma/servicos/service/ServicoService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/ServicoService.java
@@ -1,11 +1,15 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.dto.ServiceDTOS.ServiceResponseDTO;
+import com.plataforma.servicos.entity.ServiceModel;
 import com.plataforma.servicos.mapper.ServiceMapper;
 import com.plataforma.servicos.repository.CategoryRepository;
 import com.plataforma.servicos.repository.ServiceRepository;
 import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,4 +20,17 @@ public class ServicoService {
     private final CategoryRepository categoryRepository;
     private final ServiceMapper serviceMapper;
 
+
+    // Busca serviço por ID
+    // Regra: serviço desativado não pode ser visualizado
+    public ServiceResponseDTO findById(UUID id) {
+        ServiceModel service = serviceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Serviço não encontrado"));
+
+        if (Boolean.FALSE.equals(service.getAtivo())) {
+            throw new RuntimeException("Serviço não está disponível");
+        }
+
+        return serviceMapper.toResponseDTO(service);
+    }
 }
