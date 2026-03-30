cee61de78273caed3a06dc53a1c24744aa4a257c
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-30 01:13:40 -0300

    informando que essa classe é um serviço para o spring e gera constutores com argumentos para todos os campos finais da classe

--- backend-spring/src/main/java/com/plataforma/servicos/service/UserService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/UserService.java
@@ -1,4 +1,9 @@
 package com.plataforma.servicos.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
 }
