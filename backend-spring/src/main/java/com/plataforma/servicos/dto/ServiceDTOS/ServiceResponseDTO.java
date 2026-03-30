1d185ceff49334811627cf5fb5425db02458a165
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-29 21:39:39 -0300

    criando o service response para Retorna categoriaNome e prestadorNome além dos IDs → para o frontend não precisa fazer segunda requisição mediaAvaliacao → os campo calculado que o cliente vê na listagem Sem campos sensíveis internos

---
+++ backend-spring/src/main/java/com/plataforma/servicos/dto/ServiceDTOS/ServiceResponseDTO.java
@@ -0,0 +1,24 @@
+package com.plataforma.servicos.dto.ServiceDTOS;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ServiceResponseDTO(
+
+        UUID id,
+        String titulo,
+        String descricao,
+        BigDecimal preco,
+        UUID categoriaId,
+        String categoriaNome,
+        UUID prestadorId,
+        String prestadorNome,
+        String localizacao,
+        String telefoneContato,
+        Boolean ativo,
+        Double mediaAvaliacao,
+        LocalDateTime criadoEm,
+        LocalDateTime atualizadoEm
+
+) { }
