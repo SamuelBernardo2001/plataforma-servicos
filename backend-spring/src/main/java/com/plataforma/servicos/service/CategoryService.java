f02e515b4e23a56ab8bc6c7a2551716ce296b6a5
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-31 18:23:37 -0300

    chamando os repositórios e o mapper para realizar as operações de CRUD e mapeamento de entidades

--- backend-spring/src/main/java/com/plataforma/servicos/service/CategoryService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/CategoryService.java
@@ -1,9 +1,16 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.mapper.CategoryMapper;
+import com.plataforma.servicos.repository.CategoryRepository;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+    private final CategoryMapper categoryMapper;
 }
