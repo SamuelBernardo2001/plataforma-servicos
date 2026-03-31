14a75381a7b502b7dbb937cd5af6e6e24a7aa1f2
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-31 19:26:56 -0300

    chamando os repositórios e o mapper para realizar as operações de CRUD e mapeamento de entidades

--- backend-spring/src/main/java/com/plataforma/servicos/service/MessageService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/MessageService.java
@@ -1,9 +1,18 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.mapper.MessageMapper;
+import com.plataforma.servicos.repository.MessageRepository;
+import com.plataforma.servicos.repository.ServiceOrderRepository;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+    private final ServiceOrderRepository serviceOrderRepository;
+    private final UserRepository userRepository;
+    private final MessageMapper messageMapper;
 }
