6e050d7aaa24eea24d00f55d436d09f8b6312121
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-31 19:12:32 -0300

    criando a classe de serviço para os relatórios, onde serão implementados os métodos para gerar os relatórios necessários para a plataforma.

---
+++ backend-spring/src/main/java/com/plataforma/servicos/service/ReportService.java
@@ -0,0 +1,9 @@
+package com.plataforma.servicos.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+}
