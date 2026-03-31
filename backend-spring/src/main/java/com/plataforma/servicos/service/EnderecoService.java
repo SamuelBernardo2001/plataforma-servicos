b5a1faa715d63f2ced90382df18f258fc0f8f991
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-31 18:44:48 -0300

    chamando os repositórios e o mapper para realizar as operações de CRUD e mapeamento de entidades

--- backend-spring/src/main/java/com/plataforma/servicos/service/EnderecoService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/EnderecoService.java
@@ -1,9 +1,18 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.mapper.EnderecoMapper;
+import com.plataforma.servicos.repository.EnderecoRepository;
+import com.plataforma.servicos.repository.ServiceOrderRepository;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EnderecoService {
+
+    private final EnderecoRepository enderecoRepository;
+    private final UserRepository userRepository;
+    private final ServiceOrderRepository serviceOrderRepository;
+    private final EnderecoMapper enderecoMapper;
 }
