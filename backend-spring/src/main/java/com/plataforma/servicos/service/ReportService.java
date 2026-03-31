7278aa15c8abf25f139030f831cf2a7afa0ec1f5
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-31 19:13:54 -0300

    chamando os repositórios e o mapper para realizar as operações de CRUD e mapeamento de entidades

--- backend-spring/src/main/java/com/plataforma/servicos/service/ReportService.java
+++ backend-spring/src/main/java/com/plataforma/servicos/service/ReportService.java
@@ -1,9 +1,18 @@
 package com.plataforma.servicos.service;
 
+import com.plataforma.servicos.mapper.ReportMapper;
+import com.plataforma.servicos.repository.ReportRepository;
+import com.plataforma.servicos.repository.ServiceOrderRepository;
+import com.plataforma.servicos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReportService {
+
+    private final ReportRepository reportRepository;
+    private final UserRepository userRepository;
+    private final ServiceOrderRepository serviceOrderRepository;
+    private final ReportMapper reportMapper;
 }
